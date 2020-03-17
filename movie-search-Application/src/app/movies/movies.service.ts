58562ff5c35c0f1ddbef023ba988ce0b96916385
jgverges <55912813+jgverges@users.noreply.github.com>
2020-03-17 18:23:30 +0100

    To DO: movies acts favorites

--- movie-search-Application/src/app/movies/movies.service.ts
+++ movie-search-Application/src/app/movies/movies.service.ts
@@ -9,75 +9,26 @@ import { Movie } from '../models/movie';
 })
 
 export class MoviesService {
-  addItem:boolean= false;
-  public favorites: Array<{name:string,count:number}>= [{name:"camello",count:99},{name:"patata",count:77},{name:"",count:null}];
+                      addItem:boolean= false;
   hasResults:boolean = false;
   movies: [] ;
-  starOn:boolean= false;
-/*   items=[{name:"camello",count:9},{name:"pa",count:10},{name:"",count:null}];
- */
+              starOn:boolean= false;
+
   constructor(public http: HttpClient) { }
-
-/*   setFavoritesItems(favoritesItems){
-    this.items.push(favoritesItems);
-  }
-  getFavoritesItems(){
-    this.items.sort(function (a, b) {
-      if (a.count > b.count) return -1;
-      if (a.count < b.count) return 1;     
-      return 0;
-    });
-
-    return this.items;
-  }
- */  getMovies(movieTitle:any){
+ 
+  getMovies(movieTitle:any){
     this.hasResults = false;
     this.getByTitle(movieTitle.value).subscribe(data =>{
       if (data['Response']=="True"){
         this.movies=data['Search'];
         this.hasResults = true;
         this.addItem=false;
-        //console.log(this.movies, this.favorites[0].count);/*  */
-        this.incresasesFavorites(movieTitle);
-          }
+                                        console.log(this.movies);/*  */
+        }
     });
-    return this.favorites;/*  */
   }
+  
   getByTitle(movieTitle:string):Observable<Movie> {
     return this.http.get<Movie>('http://www.omdbapi.com/?apikey=90493536&s='+movieTitle);
   }
-  /* ****** */
-  getByImdbIDd(imdbID:string):Observable<Movie> {
-    return this.http.get<Movie>('http://www.omdbapi.com/?apikey=90493536&i='+imdbID);
-  }
-  /* * */
-  getFav(favoritesS){      console.warn(favoritesS);/*  */
-    favoritesS=this.favorites; console.warn(favoritesS);/*  */
-    return favoritesS;
-  }
-  incresasesFavorites(movieTitle:any){
-/*     this.favorites.push({name:movieTitle.value, count:1});console.log(this.favorites);
-    return this.favorites;/*  */  
-    let exist= false;
-    if (this.hasResults)  {                           
-        this.starOn=true; 
-        this.favorites.forEach(item =>{ 
-          if (item.name ==movieTitle.value){
-            exist=true;
-            item.count++;
-          }      
-        });    console.warn(this.favorites);         // this.favorites.push({name:movieTitle.value, count:1});console.warn(this.favorites);/* ***** */
-        if(!exist && this.addItem) {         // console.log("exist= "+movieTitle.value);
-          this.favorites.push({name:movieTitle.value, count:1});
-        } 
-    }
-    else return;
-    this.favorites.sort(function (a, b) {
-      if (a.count > b.count) return -1;
-      if (a.count < b.count) return 1;     
-      return 0;
-    });
-    return this.favorites;/*  */
-  }
-
 }
